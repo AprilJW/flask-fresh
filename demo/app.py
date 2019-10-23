62f681dac2e4742c94f6b954ac043492dadd3a48
ivy-1996 <919624032@qq.com>
2019-10-23 23:42:49 +0800

    update project body

--- demo/app.py
+++ demo/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from utils.core import DemoInit
+import importlib
 from . import config
+from .config import INSTALL_APPS
 
 app = Flask(
     __name__,
@@ -13,6 +14,52 @@ app.config.from_object(config)
 
 def create_app():
     return app
+
+
+class DemoInit(object):
+    def __init__(self, app):
+        self.app = app
+
+    @staticmethod
+    def import_modules():
+        for module in INSTALL_APPS:
+            module = '%s.models' % module
+            importlib.import_module(module)
+
+    def register_blue_print(self):
+        '''从install_app里注册蓝图'''
+        for module in INSTALL_APPS:
+            module = '%s.urls' % module
+            url = importlib.import_module(module)
+            try:
+                blue_print = getattr(url, 'blue_print')
+            except AttributeError:
+                raise AttributeError('you must define your blue_print as "blue_print" name')
+            self.app.register_blueprint(blue_print)
+
+    def initialize_admin(self):
+        '''初始化admin'''
+        for module in INSTALL_APPS:
+            module = '%s.admin' % module
+            importlib.import_module(module)
+
+    def initialize_template_tags(self):
+        '''初始化模板标签文件'''
+        try:
+            from . import template_tags
+        except ImportError as e:
+            raise e
+
+    def init(self, *args, **kwargs):
+        '''初始化全局配置'''
+        self.import_modules()
+        self.register_blue_print()
+        self.initialize_admin()
+        self.initialize_template_tags()
+        try:
+            from . import extentions
+        except ImportError as e:
+            raise e
 
 
 demo = DemoInit(app)
--- demo/config.py
+++ demo/config.py
@@ -29,13 +29,14 @@ SESSION_TYPE = "redis"
 # 设置redis的ip,port,有效时间
 REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
+REDIS_PASSWORD = ''
 # 是否强制加盐，混淆session
 SESSION_USE_SIGNER = True
 # 如果加盐，那么必须设置的安全码，盐
 SECRET_KEY = "BjyXUCVnmogYAlBLV9dLxUGK8WtyymyCx1F/E94T//Nf5O4dp2eQjWXnTzyQ4ha1UCo="
 # sessons是否长期有效，false，则关闭浏览器，session失效
 SESSION_PERMANENT = True
-SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT)
+SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, password=REDIS_PASSWORD)
 
 EMAIL_HOST = 'smtp.qq.com'
 EMAIL_USER = '919624032@qq.com'
